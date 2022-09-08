72f5dff205b5ac3b2c70366f186fc612a92f9bcf
Omuthe <billclinton19951@gmail.com>
2022-09-08 14:02:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,4 @@
 #!/bin/bash
-#include <stdio.h>
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
 int main(void)
 
 {
@@ -20,4 +8,3 @@ printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
-
