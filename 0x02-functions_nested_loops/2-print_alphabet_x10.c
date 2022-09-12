a186318ec4c9e8341b6c208d38fac2d3c4ac71c4
Omuthe <billclinton19951@gmail.com>
2022-09-12 07:30:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "print_alphabet_x10.h"
+/**
+*Prints 10 lines alphabets in lower case and adds a new line
+*/
+void print_alphabet_x10(void)
+{
+char a, b;
+for (b=0; b<10; b++)
+{
+for (a=a; a<=z;a++)
+{
+_putchar(a);
+}
+_putchar(n);
+}
+}
