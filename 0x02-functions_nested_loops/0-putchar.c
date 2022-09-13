9ed11eebd28a724c20421e268b46d7927fb278e3
Omuthe <billclinton19951@gmail.com>
2022-09-13 04:17:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "0-putchar.c.h"
+#include "main.h"
 /**
 *Prints _putchar and adds a new line
 *returns 0
 */
-int 0-putchar.c(void)
+int main(void)
 {
 _putchar(_);
 _putchar(p);
