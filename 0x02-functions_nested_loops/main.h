7c9d0e09b3cfbb16f7494c921b988c62cc8f5efc
Omuthe <billclinton19951@gmail.com>
2022-09-14 06:49:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,18 @@
-int _putchar(char c);
+#include <stdio.h>
+char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int r);
-int print_last_digit(int j);
+int _abs(int);
+int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
-int add(int i, int l);
+int add(int one, int two);
 void print_to_98(int n);
+void print_times_table(int n);
+
+
+
+
