37ad021a1be50be3b8cf4afe1acc0365f93ba332
Omuthe <billclinton19951@gmail.com>
2022-09-15 08:01:46 -0700

    101- print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - function
+ * @n: input value to check
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		putchar('-');
+	}
+	num = n;
+	num2 = num;
+	if (num > 9)
+	{
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		putchar((num2 / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			putchar((num2 / i) % 10 + '0');
+	}
+	else
+		putchar(num + '0');
+}
