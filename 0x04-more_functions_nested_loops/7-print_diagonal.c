caf44a24b927ecd003b2bb4375fbb1968e01840e
Omuthe <billclinton19951@gmail.com>
2022-09-15 07:18:03 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - Funtion
+ *
+ * @n: variable
+ */
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	if (n <= 0)
+		putchar('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				putchar('\\');
+				putchar('\n');
+			}
+			else
+			{
+				putchar(' ');
+			}
+		}
+	}
+}
