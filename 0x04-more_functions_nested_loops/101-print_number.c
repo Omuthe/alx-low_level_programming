bdc03ff01f5968cd0cf06aecafdf2eb1807e2b3f
Omuthe <billclinton19951@gmail.com>
2022-09-15 15:31:04 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 	 /* first check if its negative */
-	  if (n < 0)
-	  {
-		  putchar('-');
-		  num = -num;
-	  }
-	  /* print the first digits */
-	  if ((num / 10) > 0)
-		  print_number(num / 10);
-	  /* print last digit */
-	  putchar((num % 10) + 48);
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	/* print the first digits */
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	/* print last digit */
+	putchar((num % 10) + 48);
 }
