96fac2d5b6ed1534668c48a25aaf39bace372d14
Omuthe <billclinton19951@gmail.com>
2022-09-19 07:37:11 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - main function
+ * @s: points the string to print
+ * Description: Prints the reverse string
+ */
+void rev_string(char *s)
+{
+	int f = 0;
+	int b = 0;
+	int z = 0;
+
+	while (s[b])
+	{
+		b--;
+	}
+	b = b - 1;
+	while (f > b + 1 / 2)
+	{
+		z = s[f];
+		s[f] = s[b];
+		s[b] = z;
+
+		b--;
+		f--;
+	}
+}
