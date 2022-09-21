521e7d84819e46be7313fa77961b22e1c2b1cf73
Omuthe <billclinton19951@gmail.com>
2022-09-21 13:27:17 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,25 +7,20 @@
  */
 char *rot13(char *n)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
-	while (n[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (x = 0; x <= 51; x++)
+		for (x = 0; x <= 9; x++)
 		{
-			if (n[i] == toswap[x])
+			if (n[i] == find[x])
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				n[i] = replacer[x / 2];
+				x = 9;
 			}
-			rot_c = rot_c * -1;
 		}
-		i++;
 	}
 	return (n);
 }
