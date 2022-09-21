c1aec5e2de0ae5e9d7aac49246f9508200e458b4
Omuthe <billclinton19951@gmail.com>
2022-09-21 12:10:35 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - function
+ * @dest: destination
+ * @src: Source
+ * @n: variable
+ * Ruturn: Destination
+ */
+/**------------------Hello------World--1||1024*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int tam = 0;
+
+	for (; dest[tam];)
+	{
+		tam++;
+}
+	while (r < n && src[r])
+	{
+		dest[tam + i] = src[r];
+		i++;
+		r++;
+	}
+	return (dest);
+}
